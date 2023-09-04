f231103f9e8ba834039cc1c8c98e6967ef94f475
vahromeevilya-widestudio <98152955+vahromeevilya-widestudio@users.noreply.github.com>
2023-09-04 17:00:18 +0300

    footer and navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
 	const handleScroll = () => {};
 	return (
 		<div className="hero">
-			<div className="flex-1pt-36 padding-x">
+			<div className="flex-1 pt-36 padding-x">
 				<h1 className="hero__title">
 					Find , book ,or rent a car — quickly and easily!
 				</h1>
